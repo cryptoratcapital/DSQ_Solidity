ea06ba461addcdd7619c08a71bf43d502c6233d6
BowTiedPickle <bowtiedpickle@protonmail.com>
2023-08-12 21:46:01 -0500

    fix: unnumbered issues for StrategyARB

--- contracts/trader/strategies/Strategy_ARB.sol
+++ contracts/trader/strategies/Strategy_ARB.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import "../diamonds/StrategyDiamond.sol";
-import "../trader/ITraderV0.sol";
 import "../trader/TraderV0_Cutter.sol";
 import "../modules/gmx/swap/GMX_Swap_Cutter.sol";
 import "../modules/gmx/position/GMX_PositionRouter_Cutter.sol";
@@ -12,11 +11,11 @@ import "../modules/camelot/lp/Camelot_LP_Cutter.sol";
 import "../modules/camelot/nftpool/Camelot_NFTPool_Cutter.sol";
 import "../modules/camelot/nitropool/Camelot_NitroPool_Cutter.sol";
 import "../modules/camelot/swap/Camelot_Swap_Cutter.sol";
+import "../modules/camelot/v3/Camelot_V3_Cutter.sol";
 import "../modules/camelot/storage/Camelot_Storage_Cutter.sol";
 import "../modules/lyra/storage/Lyra_Storage_Cutter.sol";
 import "../modules/lyra/lp/Lyra_LP_Cutter.sol";
 import "../modules/lyra/options/Lyra_Options_Cutter.sol";
-import "../modules/rysk/options/Rysk_Options_Cutter.sol";
 import "../modules/aave/Aave_Lending_Cutter.sol";
 import "../modules/traderjoe/swap/TraderJoe_Swap_Cutter.sol";
 import "../modules/traderjoe/legacy_lp/TraderJoe_Legacy_LP_Cutter.sol";
@@ -41,8 +40,6 @@ import "../modules/inch/limitorder/Inch_LimitOrder_Cutter.sol";
  *          - Lyra
  *              - Options
  *              - LP
- *          - Rysk
- *              - Options
  *          - Aave
  *              - Lending and borrowing
  *          - TraderJoe
@@ -68,11 +65,11 @@ contract Strategy_ARB is
     Camelot_NFTPool_Cutter,
     Camelot_NitroPool_Cutter,
     Camelot_Swap_Cutter,
+    Camelot_V3_Cutter,
     Camelot_Storage_Cutter,
     Lyra_Storage_Cutter,
     Lyra_LP_Cutter,
     Lyra_Options_Cutter,
-    Rysk_Options_Cutter,
     Aave_Lending_Cutter,
     TraderJoe_Swap_Cutter,
     TraderJoe_Legacy_LP_Cutter,
@@ -97,11 +94,11 @@ contract Strategy_ARB is
         cut_Camelot_NFTPool(_facets[5]);
         cut_Camelot_NitroPool(_facets[6]);
         cut_Camelot_Swap(_facets[7]);
-        cut_Camelot_Storage(_facets[8]);
-        cut_Lyra_Storage(_facets[9]);
-        cut_Lyra_LP(_facets[10]);
-        cut_Lyra_Options(_facets[11]);
-        cut_Rysk_Options(_facets[12]);
+        cut_Camelot_V3(_facets[8]);
+        cut_Camelot_Storage(_facets[9]);
+        cut_Lyra_Storage(_facets[10]);
+        cut_Lyra_LP(_facets[11]);
+        cut_Lyra_Options(_facets[12]);
         cut_Aave_Lending(_facets[13]);
         cut_TraderJoe_Swap(_facets[14]);
         cut_TraderJoe_Legacy_LP(_facets[15]);
